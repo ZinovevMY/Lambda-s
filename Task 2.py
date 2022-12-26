eb5a6cbfdd76c35cfd3420e1b84d5a8330b22b84
Mikhail <mehan@list.ru>
2022-12-26 20:54:15 +0300

    Done

--- Task 2.py
+++ Task 2.py
@@ -5,19 +5,18 @@ import random
 
 elem_number = int(input('Введите кол-во элементов списка: '))
 my_list = [random.randint(-10, 10) for i in range(elem_number)]
+revers_list = list(reversed(my_list))
 
-# mult = lambda a, b: a * b
+mult = lambda a, b: a * b
 step_count = len(my_list) / 2
-# res_list = list()
+res_list = list()
 if step_count % 2 != 0:
-   step_count += 1
-# for i in range(int(step_count)):
-#     if i == step_count and neg:
-#         res_list.append(my_list[i])
-#     else:
-#         res_list.append(mult(my_list[i], my_list[-1 - i]))
-res_list = [a*b for a, b in zip(my_list, my_list[-1::-1])]
+    step_count += 1
+for i in range(int(step_count)):
+    if i+1 == int(step_count):
+        res_list.append(my_list[i])
+    else:
+        res_list.append(mult(my_list[i], revers_list[i]))
+
 print(my_list)
 print(res_list)
-
-
